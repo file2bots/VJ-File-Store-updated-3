1025453f6e1511198de675df058134eba20574b3
fairinfo174 <fairinfo7265@gmail.com>
2025-02-23 23:06:14 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import logging, asyncio, os, re, random, pytz, aiohttp, requests, string, json, http.client
 from datetime import date, datetime
-from config import SHORTLINK_API, SHORTLINK_URL
+from config import SHORTLINK_API, SHORTLINK_URL, POST_SHORT_API, POST_SHORT_URL, POST_MODE, DIRECT_GEN, DIRECT_GEN_URL, DIRECT_GEN_DB
 from shortzy import Shortzy
 from imdb import Cinemagoer 
 from bs4 import BeautifulSoup
