e51762bc814340d9c0d1cd258c8e28b7de40119e
fairinfo174 <fairinfo7265@gmail.com>
2025-02-23 23:12:34 +0530

    Create ia_filterdb.py

---
+++ plugins/database/ia_filterdb.py
@@ -0,0 +1,18 @@
+from struct import pack
+import base64
+from pyrogram.file_id import FileId
+
+def unpack_new_file_id(new_file_id):
+    """Return file_id, file_ref"""
+    decoded = FileId.decode(new_file_id)
+    file_id = encode_file_id(
+        pack(
+            "<iiqq",
+            int(decoded.file_type),
+            decoded.dc_id,
+            decoded.media_id,
+            decoded.access_hash
+        )
+    )
+    file_ref = encode_file_ref(decoded.file_reference)
+    return file_id, file_ref
