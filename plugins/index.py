dd14444cde4c1ccae160f9f343c92fbbc57ff24c
fairinfo174 <fairinfo7265@gmail.com>
2025-02-23 23:24:21 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -8,7 +8,7 @@ from info import ADMINS
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, ChatAdminRequired, UsernameInvalid, UsernameNotModified
-from info import INDEX_REQ_CHANNEL as LOG_CHANNEL
+from config import INDEX_REQ_CHANNEL as LOG_CHANNEL
 from database.ia_filterdb import save_file
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
