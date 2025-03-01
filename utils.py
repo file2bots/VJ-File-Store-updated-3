5576c23cc5a616a9c2678dd44416cb28c1ba4249
file2bots <166150891+file2bots@users.noreply.github.com>
2025-03-02 00:50:44 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -593,3 +593,13 @@ async def delete_previous_reply(chat_id):
         except Exception as e:
             print(f"Failed to delete message: {e}")
 
+def get_size(size):
+    """Get size in readable format"""
+
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    size = float(size)
+    i = 0
+    while size >= 1024.0 and i < len(units):
+        i += 1
+        size /= 1024.0
+    return "%.2f %s" % (size, units[i])
