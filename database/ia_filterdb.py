1aa85b373d0a59c0ff59139f2f9a0e9e14bfcac5
file2bots <166150891+file2bots@users.noreply.github.com>
2025-03-02 00:21:49 +0530

    Create ia_filterdb.py

---
+++ database/ia_filterdb.py
@@ -0,0 +1,53 @@
+import motor.motor_asyncio
+from config import DB_NAME, DB_URI
+from pyrogram.file_id import FileId, encode_file_id
+from struct import pack
+
+class Database:
+    
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+
+    def new_user(self, id, name):
+        return dict(
+            id=id,
+            name=name,
+        )
+    
+    async def add_user(self, id, name):
+        user = self.new_user(id, name)
+        await self.col.insert_one(user)
+    
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return bool(user)
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+    
+    async def get_all_users(self):
+        return self.col.find({})
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+# Correctly initialize the database
+db = Database(DB_URI, DB_NAME)
+
+# Function to extract file_id from new_file_id
+def unpack_new_file_id(new_file_id):
+    """Return file_id"""
+    decoded = FileId.decode(new_file_id)
+    file_id = encode_file_id(
+        pack(
+            "<iiqq",
+            int(decoded.file_type),
+            decoded.dc_id,
+            decoded.media_id,
+            decoded.access_hash
+        )
+    )
+    return file_id
