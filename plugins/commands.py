6fe9f18994d83ea5719632f49fb6a11a78ebd571
file2bots <166150891+file2bots@users.noreply.github.com>
2025-03-07 02:09:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -495,7 +495,7 @@ async def handle_message(client, message):
                     forwarded_message = await message.copy(chat_id=DIRECT_GEN_DB)
                     file_id = str(forwarded_message.id)
 
-                    size = get_size(message.document.file_size) if message.document else "Unknown"
+                    size = await get_size(message.document.file_size) if message.document else "Unknown"
                     quality_match = re.search(r"(480p|720p|1080p|360p|720p|1080p - HEVC|7200p - HEVC)", message.caption or "", re.IGNORECASE)
                     quality = quality_match.group(1) if quality_match else None
 
