b1ca4526e9da5fc587cf3f46b6e8ab0db0c3228d
file2bots <166150891+file2bots@users.noreply.github.com>
2025-03-08 01:29:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -108,7 +108,13 @@ async def start(client, message):
                     btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start={message.command[1]}")])
                 else:
                     btn.append([InlineKeyboardButton("♻️ Try Again ♻️", url=f"https://t.me/{username}?start=true")])
-                await message.reply_text(text=f"<b>👋 Hello {message.from_user.mention},\n\nPlease join the channel then click on try again button. 😇</b>", reply_markup=InlineKeyboardMarkup(btn))
+                #await message.reply_text(text=f"<b>👋 Hello {message.from_user.mention},\n\nPlease join the channel then click on try again button. 😇</b>", reply_markup=InlineKeyboardMarkup(btn))
+                await client.send_photo(
+                    chat_id=message.from_user.id,
+                    photo=random.FORCESUB_IMG,
+                    caption=script.FORCESUB_TEXT.format(message.from_user.mention, me.mention),
+                    reply_markup=InlineKeyboardMarkup(btn)
+                )
                 return
         except Exception as e:
             print(e)
