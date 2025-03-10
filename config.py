c34f1882341d51c963acaa004d9483eadfd69040
file2bots <166150891+file2bots@users.noreply.github.com>
2025-03-10 17:00:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -101,9 +101,12 @@ POST_SHORT_URL = environ.get('POST_SHORT_URL', 'modijiurl.com')
 HOW_TO_POST_SHORT = environ.get('HOW_TO_POST_SHORT', 'https://t.me/Howtodowloa/13')
 
 # List of target channels for posting
-TARGET_CHANNELS = {
-    -1002308357802: "PKpkpkMy Backup Here",  # Replace with your actual channel ID and name
-    -1001842318978: "Post updated channel",  # Add more channels as needed
-}
+#TARGET_CHANNELS = {
+    #-1002308357802: "PKpkpkMy Backup Here",  # Replace with your actual channel ID and name
+    #-1001842318978: "Post updated channel",  # Add more channels as needed
+#}
+
+AUTO_POST_CHANNEL = int(os.environ.get("AUTO_POST_CHANNEL", "-1002308357802"))
+SECONDARY_POST_CHANNEL = int(os.environ.get("SECONDARY_POST_CHANNEL", "-1001842318978"))
 
 
