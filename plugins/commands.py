84277932e7aac828e2595751572f3e71d61a253c
file2bots <166150891+file2bots@users.noreply.github.com>
2025-03-11 15:59:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -624,8 +624,7 @@ import re
 import os
 import json
 import base64
-from utils import get_poster #
-from plugins import genlink  # Ensure genlink is imported
+
 
 
 async def allowed(_, __, message):
